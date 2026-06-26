2f1c67e9db6a0415da11cae8cb7a3e96d6e6f2c4
ragav <ragavvignes2005@gmail.com>
2026-06-26 22:20:22 +0530

    Hands-on 2

---
+++ PythonBackendFrameworks/courses/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Course, Department, Enrollment, Student
+
+admin.site.register(Department)
+admin.site.register(Student)
+admin.site.register(Enrollment)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["name", "code", "credits", "department"]
+
+    search_fields = ["name", "code"]
+
+    list_filter = ["department"]
