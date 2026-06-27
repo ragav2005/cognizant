78a2e340fd3a04d680d1291f890be980c71d789d
ragav <ragavvignes2005@gmail.com>
2026-06-27 15:14:12 +0530

    feat: hands-on 4

---
+++ PythonBackendFrameworks/flask_coursemanager/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = "t+p-b9&oq@xro@gc9s8i3ckl(+twpbe@+m#7t+&we@7sre"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///coursemanager.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DEBUG = True
